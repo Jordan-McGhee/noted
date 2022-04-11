d0029be4cfa6fe66f9baf22ce651ba65cf8518d5
Jordan McGhee <jordanmcghee95@gmail.com>
2022-04-11 19:31:54 -0400

    Finished mock-up styling on home page post cards

--- src/App.js
+++ src/App.js
@@ -5,11 +5,11 @@ import PostList from './Home/Components/Posts/PostList';
 function App() {
 
   const DUMMY_POSTS = [
-    { id: "post1", user: { name: "Jordan McGhee", email: "test@test"}, content: "This is a test post", date: "placeholder for date text"},
+    { id: "post1", user: { name: "Jordan McGhee", email: "test@test"}, content: "This is a test post This is a test post This is a test post This is a test post This is a test post This is a test post This is a test post This is a test post This is a test post This is a test post This is a test post This is a test post This is a test post This is a test post This is a test post This is a test post This is a test post This is a test post This is a test post This is a test post This is a test post This is a test post This is a test post This is a test post This is a test post ", date: "placeholder for date text"},
     { id: "post2", user: { name: "Tori McGhee", email: "test1@test"}, content: "This is not a test post", date: "placeholder for date text"},
     { id: "post3", user: { name: "Rhonda McGhee", email: "test2@test"}, content: "This is Mom's test post", date: "placeholder for date text"},
     { id: "post4", user: { name: "Thomas McGhee", email: "test3@test"}, content: "This is Dad's test post", date: "placeholder for date text"},
-    { id: "post1", user: { name: "Chris McGhee", email: "test4@test"}, content: "This is Ballin C's test post", date: "placeholder for date text"}
+    { id: "post5", user: { name: "Chris McGhee", email: "test4@test"}, content: "This is Ballin C's test post", date: "placeholder for date text"}
   ]
 
   return (
@@ -28,7 +28,7 @@ function App() {
         </div>
 
       </div>
-      
+
     </div>
   );
 }
