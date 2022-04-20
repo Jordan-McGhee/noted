01f13c7181552cea1a25fa35f0b58a7b485a1c4d
Jordan McGhee <jordanmcghee95@gmail.com>
2022-04-19 20:07:09 -0400

    Started on auth page. Basic form 75% done

--- src/Pages/Auth/Auth.js
+++ src/Pages/Auth/Auth.js
@@ -1,9 +1,92 @@
-import React from "react";
+import React, { useState } from "react";
+
+import "./Auth.css"
 
 const Auth = () => {
+
+    // FIRST
+    // NEED A LOGGING UP AND SIGN IN FORM
+    // NEED STATE TO DETERMINE WHICH FORM TO RENDER
+    // STATE CHANGE HANDLER FOR WHEN BUTTON TO SWITCH IS PRESSED  --done
+    // SUBMIT HANDLER FUNCTION
+    // CONDITIONAL RENDERING OF FORM BEFORE RETURN STATEMENT
+    
+    // NEXT
+    // VALIDATE FORM INPUTS
+    // ERROR HANDLING
+    // ONCHANGE & ONBLUR HANDLERS
+    // STATES OR REFS FOR INPUTS
+
+    const [ isLoggingIn, setIsLoggingIn ] = useState(true)
+    
+    let loginForm
+
+    if (isLoggingIn) {
+        loginForm = 
+            <div>
+
+                <div className="form-field">
+                    <label>Username/Email</label>
+                    <input type="text" />
+                </div>
+
+                <div className="form-field">
+                    <label>Password</label>
+                    <input type="password" minLength="6" />
+                </div>
+                
+            </div>
+
+    } else {
+        loginForm = 
+            <div>
+                
+                <div className="form-field">
+                    <label>Username</label>
+                    <input type="text" />
+                </div>
+
+                <div className="form-field">
+                    <label>Email</label>
+                    <input type="text" />
+                </div>
+
+                <div className="form-field">
+                    <label>Password</label>
+                    <input type="password" minLength="6" />
+                </div>
+                
+            </div>
+    }
+    
+    const changeLoginHandler = () => {
+        if (isLoggingIn) {
+            setIsLoggingIn(false)
+        } else {
+            setIsLoggingIn(true)
+        }
+    }
+
+    const formSubmitHandler = (event) => {
+        event.preventDefault()
+
+
+    }
+
     return (
         <div>
-            <p>The Auth Page!</p>
+            <form onSubmit={ formSubmitHandler } className = "auth-form">
+                
+                { loginForm }
+
+                <div>
+                    <button type='submit' className="auth-form-button">
+                        { isLoggingIn ? "Login" : "Signup" }
+                    </button>
+
+                    <p onClick={ changeLoginHandler }>{ isLoggingIn ? "I need an Account" : "I already have an account"}</p>
+                </div>
+            </form>
         </div>
     )
 }
