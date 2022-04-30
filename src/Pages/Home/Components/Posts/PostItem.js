26ac7e5992bfebf3772a969ab6228b7a6c395484
Jordan McGhee <jordanmcghee95@gmail.com>
2022-04-29 21:39:40 -0400

    Added conditional rendering of comments for each postItem

--- src/Pages/Home/Components/Posts/PostItem.js
+++ src/Pages/Home/Components/Posts/PostItem.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Card from "../../../../Components/UI/Card"
 import userIcon from "../../../../Icons/user-placeholder.png"
@@ -7,6 +7,17 @@ import commentIcon from "../../../../Icons/comment-placeholder.png"
 import "./PostItem.css"
 
 const PostItem = props => {
+
+    const [ showComments, setShowComments ] = useState(false)
+
+    const showCommentHandler = () => {
+        if (showComments) {
+            setShowComments(false)
+        } else {
+            setShowComments(true)
+        }
+    }
+
     return (
         <Card className="post-card">
 
@@ -40,10 +51,18 @@ const PostItem = props => {
                         <p>12</p>
                     </div>
 
-                    <div className="post-view-comments">
-                        <p>View Comments</p>
+                    <div className="post-view-comments" onClick={ showCommentHandler }>
+                        
+                        <p>{ showComments ? "Hide Comments" : "View Comments"}</p>
+                        
                     </div>
                 </div>
+
+                { showComments &&
+                    <div className='post-comment-section'>
+                        <h3>Comments!</h3>
+                    </div>
+                }
 
             </div>
         </Card>
