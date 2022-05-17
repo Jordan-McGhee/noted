8a60d54e1bdaf001a51f82363e7d1f667a5a53fc
Jordan McGhee <jordanmcghee95@gmail.com>
2022-05-17 19:12:37 -0400

    Fleshing out backend. Need to connect to MongoDB server, but their site is having issues rn

---
+++ Backend/server.js
@@ -0,0 +1,56 @@
+import fs from "fs"
+import path from "path"
+
+import express from "express"
+import bodyParser from "body-parser"
+import mongoose from "mongoose"
+
+// FOR CONNECTING TO MONGODB SERVER
+const password = ""
+const url = ``
+
+// ROUTE VARIABLES
+const authRoutes = require("./routes/auth-routes")
+const userRoutes = require("./routes/user-routes")
+const postRoutes = require("./routes/post-routes")
+const HttpError = require("./models/http-error")
+
+const app = express()
+
+app.use(bodyParser.json())
+
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*")
+    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-Width, Content-Type, Accept, Authorization")
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE")
+    
+    next()
+})
+
+app.use("/auth", authRoutes)
+
+app.use("/user", userRoutes)
+
+app.use("/post", postRoutes)
+
+app.use((req, res, next) => {
+    throw new HttpError("Could not find this route.", 404)
+})
+
+// If we reach this middleware, that means we haven't returned a response on any of the above middleware routes
+app.use((error, req, res, next) => {
+
+    if (req.file) {
+        fs.unlink(req.file.path, (err) => {
+            console.log(err)
+        })
+    }
+
+    if (res.headerSent) {
+        return next(error);
+    }
+
+    // set the status and message unless there is one attached already
+    res.status(error.code || 500);
+    res.json({message: error.message || "An unknown error occurred!"});
+})
