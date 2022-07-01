9fd395fdd1a68da560438dc71e3b051f4d2d2f4d
Jordan McGhee <jordanmcghee95@gmail.com>
2022-07-01 19:39:12 -0400

    Lot of changes to login/signup form. Added states to track user input for username, email, and password. Checked validity and added onBlur handlers. Frontend connected to backend so a user is actually created when the form is submitted

--- src/Pages/Auth/Auth.js
+++ src/Pages/Auth/Auth.js
@@ -21,21 +21,105 @@ const Auth = () => {
     const authContext = useContext(AuthContext)
 
     const [ isLoggingIn, setIsLoggingIn ] = useState(true)
+
+    // STATES FOR LOGIN/SIGNUP FORM INPUTS TO TRACK VALUES
+    const [ enteredUsername, setEnteredUsername ] = useState("")
+    const [ enteredEmail, setEnteredEmail ] = useState("")
+    const [ enteredPassword, setEnteredPassword ] = useState("")
+
+    // STATES FOR IF INPUTS WERE TOUCHED, USED TO CHECK IF INPUTS ARE VALID BEFORE MOVING ON
+    const [ usernameTouched, setUsernameTouched ] = useState(false) 
+    const [ emailTouched, setEmailTouched ] = useState(false) 
+    const [ passwordTouched, setPasswordTouched ] = useState(false)
+
+    // variables to track validity and errors in inputs to prevent user from submitting form
+    const usernameIsValid = enteredUsername.trim() !== ""
+    const emailIsValid = enteredEmail.trim() !== "" && enteredEmail.includes("@")
+    const passwordIsValid = enteredPassword.trim() !== "" && enteredPassword.trim().length > 6
+
+    const usernameHasError = !usernameIsValid && usernameTouched
+    const emailHasError = !emailIsValid && emailTouched
+    const passwordHasError = !passwordIsValid && passwordTouched
+
+    // variables for styling individual inputs depending on their validity
+    const usernameInputClasses = !usernameHasError ? "form-control" : "form-control invalid"
+    const emailInputClasses = !emailHasError ? "form-control" : "form-control invalid"
+    const passwordInputClasses = !passwordHasError ? "form-control" : "form-control invalid"
+
+    // determines if all inputs in form are valid for styling/preventing form submission
+    let formIsValid = false
+
+    // check whether logging in or signing up to determine form validity
+    if (isLoggingIn) {
+        if (emailIsValid && passwordIsValid) {
+            formIsValid = true
+        }
+    } else {
+        if (usernameIsValid && emailIsValid && passwordIsValid) {
+            formIsValid = true
+        }
+    }
+
+
+    // value change handlers
+    const usernameChangeHandler = event => {
+        setEnteredUsername(event.target.value)
+    }
+
+    const emailChangeHandler = event => {
+        setEnteredEmail(event.target.value)
+    }
+
+    const passwordChangeHandler = event => {
+        setEnteredPassword(event.target.value)
+    }
+
+    // input blur handlers
+    const usernameBlurHandler = () => {
+        setUsernameTouched(true)
+    }
     
+    const emailBlurHandler = () => {
+        setEmailTouched(true)
+    }
+    
+    const passwordBlurHandler = () => {
+        setPasswordTouched(true)
+    }
+
+    // reset form handler
+    const resetFormHandler = () => {
+        setEnteredUsername("")
+        setEnteredEmail("")
+        setEnteredPassword("")
+
+        setUsernameTouched(false)
+        setEmailTouched(false)
+        setPasswordTouched(false)
+    }
+    
+
+    // establish form variable and store different version in form depending on logging in or signing up
     let loginForm
 
     if (isLoggingIn) {
         loginForm = 
             <div>
 
+                {/* EMAIL INPUT */}
                 <div className="form-field">
                     <label htmlFor="email" >Email</label>
-                    <input type="text" name="email"/>
+                    <input id="email" type="text" name="email" value={ enteredEmail } onChange = { emailChangeHandler } onBlur = { emailBlurHandler }/>
+
+                    { emailHasError && <p className = "error-text">Please enter a valid email.</p> }
                 </div>
 
+                {/* PASSWORD INPUT */}
                 <div className="form-field">
-                    <label>Password</label>
-                    <input type="password" minLength="6" name="password" />
+                    <label htmlFor="password" >Password</label>
+                    <input id="password" type="password" minLength="6" name="password" value={ enteredPassword } onChange = { passwordChangeHandler } onBlur = { passwordBlurHandler }/>
+
+                    { passwordHasError && <p className = "error-text">Please enter a valid password. Must be 6 characters or longer</p> }
                 </div>
                 
             </div>
@@ -44,34 +128,53 @@ const Auth = () => {
         loginForm = 
             <div>
                 
+                {/* USERNAME INPUT */}
                 <div className="form-field">
-                    <label>Username</label>
-                    <input type="text" name="username" />
+                    <label htmlFor="username" >Username</label>
+                    <input id="username" type="text" name="username" value={ enteredUsername } onChange = { usernameChangeHandler } onBlur = { usernameBlurHandler }/>
+
+                    { usernameHasError && <p className = "error-text">Please enter a valid username.</p> }
                 </div>
 
+                {/* EMAIL INPUT */}
                 <div className="form-field">
                     <label htmlFor="email">Email</label>
-                    <input type="text" name="email" />
+                    <input id="email" type="text" name="email" value={ enteredEmail } onChange = { emailChangeHandler } onBlur = { emailBlurHandler } />
+
+                    { emailHasError && <p className = "error-text">Please enter a valid email.</p> }
                 </div>
 
+                {/* PASSWORD INPUT */}
                 <div className="form-field">
-                    <label>Password</label>
-                    <input type="password" minLength="6" name="password" />
+                    <label htmlFor="password" >Password</label>
+                    <input id="password" type="password" minLength="6" name="password" value={ enteredPassword } onChange = { passwordChangeHandler } onBlur = { passwordBlurHandler }/>
+
+                    { passwordHasError && <p className = "error-text">Please enter a valid password. Must be 6 characters or longer</p> }
                 </div>
                 
             </div>
     }
-    
+
+
+    // switches between login and signup forms
     const changeLoginHandler = () => {
         if (isLoggingIn) {
             setIsLoggingIn(false)
+            resetFormHandler()
         } else {
             setIsLoggingIn(true)
+            resetFormHandler()
         }
     }
 
+    // when the form is submitted
     const formSubmitHandler = async (event) => {
         event.preventDefault()
+
+        // if the form is invalid and was somehow submitted, cancel form submission
+        if (!formIsValid) {
+            return
+        }
 
         if (isLoggingIn) {
 
@@ -83,9 +186,9 @@ const Auth = () => {
                         'Content-Type': "application/json"
                     },
                     body: JSON.stringify({
-                        username: event.username.value,
-                        email: event.email.value,
-                        password: event.password.value
+                        username: enteredUsername,
+                        email: enteredEmail,
+                        password: enteredPassword
                     })
                 })
 
@@ -96,6 +199,7 @@ const Auth = () => {
             }
         }
 
+        resetFormHandler()
         authContext.login()
     }
 
@@ -111,7 +215,7 @@ const Auth = () => {
                 { loginForm }
 
                 <div>
-                    <button type='submit' className="auth-form-button">
+                    <button type='submit' className="auth-form-button" disabled = { !formIsValid } onClick={ () => console.log("Clicked!")}>
                         { isLoggingIn ? "Login" : "Signup" }
                     </button>
 
